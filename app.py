f48b69d14927d1eb7840053b1566d9990c7a563e
Sergio Victor Orizano Salvador <sorizano@continental.edu.pe>
2026-03-16 20:02:02 -0500

    creando la clase con las funciones

--- app.py
+++ app.py
@@ -1,1 +1,35 @@
-print("Hola Mundo")
+import streamlit as st
+
+
+# ----------------------------------------
+# CLASE (POO)
+# ----------------------------------------
+
+class Cliente:
+    def __init__(self, nombre, edad, saldo):
+        self.__nombre = nombre
+        self.__edad = edad
+        self.__saldo = saldo
+
+    #GETTERS (encapsulamiento)
+    def get_nombre(self):
+        return self.__nombre
+    
+    def get_edad(self):
+        return self.__edad
+
+    def get_saldo(self):
+        return self.__saldo
+  
+
+    # Método
+    def mostrar_info(self):
+        return f"Cliente: {self.__nombre}, Edad: {self.__edad}, Saldo: S/ {self-__saldo}"
+
+# ----------------------------------------
+# STREAMLIT
+# ----------------------------------------
+
+st.title("Demo POO - Ciencia de Datos")
+
+st.write("Ingrese los datos del cliente:")
