a331c20cef18120453049d2f87d10a12049d29c3
Sergio Victor Orizano Salvador <sorizano@continental.edu.pe>
2026-03-23 20:50:58 -0500

    mostrando clientes creados en tabla

--- app.py
+++ app.py
@@ -6,14 +6,16 @@ from services.cliente_service import crear_cliente
 st.title("Demo POO - Ciencia de Datos")
 st.write("Ingrese los datos del cliente:")
 
+# Inputs
 nombre = st.text_input("Nombre")
 edad = st.number_input("Edad", min_value = 0)
 saldo = st.number_input("Saldo", min_value = 0.0)
 
+# Botón
 if st.button("Crear Cliente"):
 
     try:
-        #Secuencia
+        # Crea Cliente + Mensaje
         cliente, mensaje = crear_cliente(nombre, edad, saldo)
 
         st.success("Cliente creado correctamente")
@@ -23,15 +25,17 @@ if st.button("Crear Cliente"):
         st.write("### Información del cliente: ")
         st.write(cliente.mostrar_info())
 
-        if hasattr(cliente, "clasificar_cliente"):
-            st.write("### Clasificación:")
-            st.write(cliente.clasificar_cliente())
-        
-        if hasattr(cliente, "es_mayor_edad"):
-            if cliente.es_mayor_edad():
-                st.success("Es mayor de edad")
-            else:
-                st.warning("Es menor de edad")
+        data = {
+            "Nombre": [cliente.get_nombre()],
+            "Edad": [cliente.get_edad()],
+            "Saldo": [cliente.get_saldo]
+        }
+
+        df = pd.DataFrame(data)
+
+        st.write("### Tabla del Cliente")
+        st.dataframe(df)
+
     
     except Exception as e:
         st.error(str(e))
