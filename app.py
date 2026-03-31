5b204abce06049c1cc1d06c48503854129438a25
Sergio Victor Orizano Salvador <sorizano@continental.edu.pe>
2026-03-30 20:30:18 -0500

    agregando mas reglas en app.py

--- app.py
+++ app.py
@@ -20,23 +20,53 @@ if st.button("Crear Cliente"):
         cliente, mensaje = crear_cliente(nombre, edad, saldo)
 
         st.success("Cliente creado correctamente")
-
         st.info(mensaje)
 
-        st.write("### Información del cliente: ")
-        st.write(cliente.mostrar_info())
-
-        data = {
-            "Nombre": [cliente.get_nombre()],
-            "Edad": [cliente.get_edad()],
-            "Saldo": [cliente.get_saldo()]
-        }
-
-        df = pd.DataFrame(data)
-
-        st.write("### Tabla del Cliente")
-        st.dataframe(df)
-
+        #Guardar en Memoria
+        st.session_state.clientes.append({
+            "Nombre": cliente.get_nombre(),
+            "Edad": cliente.get_edad(),
+            "Saldo": cliente.get_saldo()
+        })
     
     except Exception as e:
         st.error(str(e))
+
+#Mostrar tabla acomulada
+if len(st.session_state.clientes) > 0:
+
+    st.write("### Tabla de Clientes")
+    df = pd.DataFrame(st.session_state.clientes)
+    st.dataframe(df)
+
+    # FOR -> recorrer clientes
+    st.write("### Lista de clientes (for)")
+    for c in st.session_state.clientes:
+        st.write(f"{c['Nombre']} - S/ {c['Saldo']}")
+
+
+    # WHILE -> recorrido alternativo
+    st.write("### Recorrido con While")
+    i = 0
+    while i < len(st.session_state.clientes):
+        c = st.session_state.clientes[i]
+        st.write(f"{c['Nombre'] - Edad: {c['Edad']}}")
+        i += 1
+    
+    # Análisis: Total de clientes
+    contador = 0
+    for c in st.session_state.clientes:
+        contador += 1
+    
+    st.write(f"Total de Clientes: {contador}")
+
+    # Promedio de saldo
+    suma = 0
+    for c in st.session_state.clientes:
+        suma += C["Saldo"]
+    
+    promedio = suma / contador
+    st.write(f"Promedio de saldo: S/ {round(promedio, 2)}")
+
+else:
+    st.warning("Aún no hay clientes registrados")
