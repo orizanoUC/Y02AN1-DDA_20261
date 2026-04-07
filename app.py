ea227323550d8bf681db2b7c5f07b634b27c3d86
Sergio Victor Orizano Salvador <sorizano@continental.edu.pe>
2026-04-06 20:39:10 -0500

    bug en saldo negativo, quitando min value

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ with st.form("form_cliente"):
     # Inputs
     nombre = st.text_input("Nombre")
     edad = st.number_input("Edad", min_value = 0)
-    saldo = st.number_input("Saldo", min_value = 0.0)
+    saldo = st.number_input("Saldo")
 
     submitted = st.form_submit_button("Crear Cliente")
 
