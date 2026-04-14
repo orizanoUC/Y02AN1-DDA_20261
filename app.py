64fc5227d94ac4ef1fbe96785f7a5a72c65384b0
Sergio Victor Orizano Salvador <sorizano@continental.edu.pe>
2026-04-13 20:39:23 -0500

    CRUD con base de datos bug

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import streamlit as st
-from models.cliente import Cliente
 from utils.form_utils import procesar_cliente
-from services.cliente_service import crear_cliente
 from utils.cliente_utils import (
     mostrar_tabla,
     mostrar_clientes,
@@ -11,23 +9,29 @@ from utils.cliente_utils import (
 st.title("Demo POO - Ciencia de Datos")
 st.write("Ingrese los datos del cliente:")
 
-
 if "clientes" not in st.session_state:
     st.session_state.clientes = []
 
 with st.form("form_cliente"):
+
     nombre = st.text_input("Nombre")
-    edad = st.number_input("Edad", min_value = 0)
+    edad = st.number_input("Edad", min_value=0)
     saldo = st.number_input("Saldo")
+
     submitted = st.form_submit_button("Crear Cliente")
 
     if submitted:
         procesar_cliente(nombre, edad, saldo)
 
+#  Mostrar datos (memoria)
 if len(st.session_state.clientes) > 0:
-    st.write("### Tabla de Clientes")
+
+    st.write("### Tabla de Clientes (Memoria)")
     mostrar_tabla(st.session_state.clientes)
+
     mostrar_clientes(st.session_state.clientes)
+
     mostrar_analisis(st.session_state.clientes)
+
 else:
     st.warning("Aún no hay clientes registrados")
--- utils/form_utils.py
+++ utils/form_utils.py
@@ -2,7 +2,6 @@ import streamlit as st
 from services.cliente_service import crear_cliente
 from services.supabase_service import insertar_cliente
 
-
 def procesar_cliente(nombre, edad, saldo):
 
     try:
@@ -19,23 +18,24 @@ def procesar_cliente(nombre, edad, saldo):
         st.success("Cliente creado correctamente")
         st.info(mensaje)
 
-        #Guardar
         cliente_dict = {
-            "Nombre": cliente.get_nombre(),
-            "Edad": cliente.get_edad(),
-            "Saldo": cliente.get_saldo()
+            "nombre": cliente.get_nombre(),
+            "edad": cliente.get_edad(),
+            "saldo": cliente.get_saldo()
         }
 
-        st.session_state.cliente.append ({
+        # Guardar en memoria
+        st.session_state.clientes.append({
             "Nombre": cliente.get_nombre(),
             "Edad": cliente.get_edad(),
             "Saldo": cliente.get_saldo()
         })
-        
+
+        # Guardar en Supabase
         insertar_cliente(cliente_dict)
-        
+
     except ValueError as e:
         st.warning(str(e))
 
-    except Exception:
+    except Exception as e:
         st.error("Error inesperado: " + str(e))
